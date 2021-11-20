19d17c53ce5676b08565055710af551ef2d35735
hetjagani <hetpatel572@gmail.com>
2021-11-20 10:28:05 -0800

    fixed logic to land player on the top of brick

--- escapethecastle/Brick.java
+++ escapethecastle/Brick.java
@@ -8,19 +8,14 @@ import greenfoot.GreenfootImage;
  * @version (a version number or a date)
  */
 public class Brick extends DisplayComponent {
-    /**
-     * Act - do whatever the Brick wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
-
-    private final int GRAVITY = 1;
-    private int velocity;
+    private int vSpeed = 0;
+    private int gravity = 2;
     
     public Brick(int velocity) {
         GreenfootImage img = new GreenfootImage(getImage());
         img.scale(img.getWidth() / 5, img.getHeight() / 5);
         setImage(img);
-        this.velocity = velocity;
+        this.vSpeed = velocity;
     }
 
     public void act() {
@@ -28,7 +23,7 @@ public class Brick extends DisplayComponent {
     }
 
     public void fall() {
-        setLocation(getX(), getY() + velocity);
-        if (getY() > getWorld().getHeight() - getImage().getHeight() / 2 - 1) velocity = 0;
+        setLocation(getX(), getY() + vSpeed);
+        if (getY() >= getWorld().getHeight() - getImage().getHeight() / 2) vSpeed = 0;
     }
 }
